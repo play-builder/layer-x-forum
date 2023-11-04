018647aa9aae90cd302cfa7d0560eb46cf27b20e
play-builder <playbuilder47@gmail.com>
2023-11-04 19:56:19 +0900

    feat(infra): Configure AWS Provider

---
+++ terraform/main.tf
@@ -0,0 +1,47 @@
+# ================================
+# LayerX Forum - Terraform Main
+# ================================
+
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+    }
+  }
+}
+
+resource "random_password" "db_password" {
+  length           = 32
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "random_password" "jwt_secret" {
+  length  = 64
+  special = false
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_caller_identity" "current" {}
